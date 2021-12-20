c1e94ad34acfd5680f4d8f62b33a1bc2532cf88e
Wenjie Fang <wefang@microsoft.com>
2021-12-20 14:43:42 -0800

    Refactor.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import yanxin from './yanxin.jpg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Message from './Message';
+import React from 'react';
+import Header from "./Header";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-      <p>
-          庄凡心生日快乐！
-        </p>
-        <img class="fanxin" src={yanxin} alt="fanxin"/>
-        <br/>
-        <a
-          className="App-link"
-          href="https://huati.weibo.com/5769150"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          岑北南超话
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      <Message />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css';
+import Message from './Message';
+import yanxin from './yanxin.jpg';
+
+function Header() {
+    return (
+        <div className="App">
+    <span>
+        庄凡心生日快乐！
+      </span>
+      <a
+        className="App-link"
+        href="https://huati.weibo.com/5769150"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        岑北南超话
+      </a>
+  </div>
+    )
+}
+
+export default Header
